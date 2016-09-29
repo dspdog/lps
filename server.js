ee2eea364c5cdd1046294e350bc34345cbb59b1f
Pawel Mikolajczyk <pmikolajczk@WALpmikolajczyk9BG8WM.fios-router.home>
2016-09-28 22:42:47 -0400

    ...

---
+++ server.js
@@ -0,0 +1,56 @@
+var url = require('url');
+
+var http = require('http');
+var request = require('request');
+var util = require('util');
+
+var exec = require('child_process').exec;
+
+if (!String.prototype.format) {
+    String.prototype.format = function() {
+        var args = arguments;
+        return this.replace(/{(\d+)}/g, function(match, number) {
+            return typeof args[number] != 'undefined'
+                ? args[number]
+                : match
+                ;
+        });
+    };
+}
+
+var getPlateOutputForImage = function(picurl, cb){
+    var cmd = 'wget {0} -o lp.jpg'.format(picurl);
+    console.log("CMD1",cmd);
+
+    exec(cmd, function(error, stdout, stderr) {
+
+        cmd = 'docker run -it --rm -v $(pwd):/data:ro openalpr -c eu lp.jpg';
+
+        console.log("CMD2",cmd);
+        console.log("OUT1",stdout);
+
+        exec(cmd, function(error, stdout, stderr) {
+            // command output is in stdout
+            console.log("OUT2",stdout);
+            cb(stdout);
+        });
+    });
+}
+
+var server = http.createServer(function(req, res) {
+    var queryData = url.parse(req.url, true).query; //eg, if(queryData.eventpush || queryData.eventpusher){
+    if(queryData.picurl){
+
+        var onGotResult = function(result){
+            res.end(result);
+        };
+
+        getPlateOutputForImage(queryData.picurl, onGotResult);
+
+    }else{
+        res.end("no img provided");
+    }
+});
+
+console.log("SERVER LISTEN", 80);
+server.listen(80);
